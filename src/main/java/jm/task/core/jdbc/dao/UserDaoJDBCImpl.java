8c8b46e2954e23cd5366380c38b23c0368121292
Samummm <mogila@tut.by>
2023-06-15 16:37:59 +0300

    ver 1.0

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,95 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    private Statement stat;
 
+    public UserDaoJDBCImpl() {
+        try {
+            stat = Util.getConn().createStatement();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void createUsersTable() {
-
+        String updSQL = "CREATE TABLE IF NOT EXISTS user (" +
+                                "id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT," +
+                                "first_name VARCHAR(50) ," +
+                                "last_name VARCHAR(50)," +
+                                "age TINYINT)";
+        try {
+            stat.executeUpdate(updSQL);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        String updSQL = " DROP TABLE IF EXISTS user";
+        try {
+            stat.executeUpdate(updSQL);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String updSQL = "INSERT INTO user (first_name, last_name, age) VALUES('" +
+                name + "','" + lastName + "'," + age + ")";
+        try {
+            stat.executeUpdate(updSQL);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        System.out.printf("User с именем – %s добавлен в базу данных.\n", name);
     }
 
     public void removeUserById(long id) {
-
+        String updSQL = "DELETE FROM user WHERE id = " + id;
+        try {
+            stat.executeUpdate(updSQL);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        ResultSet results;
+        String querySQL = "SELECT * FROM user";
+        try {
+            results = stat.executeQuery(querySQL);
+            while (results.next()) {
+                User user = new User();
+                user.setId(results.getLong(1));
+                user.setName(results.getString(2));
+                user.setLastName(results.getString(3));
+                user.setAge(results.getByte(4));
+                users.add(user);
+                System.out.println(user.toString());
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        String updSQL = "TRUNCATE TABLE user";
+        try {
+            stat.executeUpdate(updSQL);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+     private static Connection conn;
+
+    public static Connection getConn() {
+        return conn;
+    }
+
+    public static void getConnection() throws SQLException, ClassNotFoundException {
+        String hostName = "localhost";
+        String dbName = "kata_db";
+        String userName = "root";
+        String password = "root";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+        Connection connection = DriverManager.getConnection(connectionURL, userName, password);
+        conn = connection;
+    }
 }
+
